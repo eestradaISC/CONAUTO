c4894163988bf1fa22553b4429d36ac80f3bfd8c
eestradaISC <33323957+eestradaISC@users.noreply.github.com>
2023-09-14 09:51:06 -0600

    chore comment operation

--- Netsuite-Conauto/con_rl_service_netsuite_conauto.js
+++ Netsuite-Conauto/con_rl_service_netsuite_conauto.js
@@ -55,7 +55,7 @@ define([
                     'ReservaPasivo': reservaPasivo,
                     'Bajas': bajaFolio,
                     'ModificacionBajas': modificacionBajas,
-                    'AplicacionCobranzaASH': AplicacionCobranza,
+                    'AplicacionCobranza': AplicacionCobranza,
                 }
                 let callback = operations[data.tipo];
                 if (callback) {
